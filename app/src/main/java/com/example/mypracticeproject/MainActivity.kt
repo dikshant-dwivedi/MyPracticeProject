7342a21bbfe5ac44f7b5492c7c7b0ec8a4bcdaba
dikshant.dwivedi <dikshantdwivedi055@gmail.com>
2022-08-17 13:19:54 +0530

    added business card app

--- app/src/main/java/com/example/mypracticeproject/MainActivity.kt
+++ app/src/main/java/com/example/mypracticeproject/MainActivity.kt
@@ -8,14 +8,20 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Call
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -47,16 +53,17 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     MyPracticeProjectTheme {
-        MyPracticeProjectApp()
+        BusinessCardApp()
     }
 }
 
 @Composable
-fun MyPracticeProjectApp(){
+fun MyPracticeProjectApp() {
     Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
         ArticleApp()
         TaskManagerApp()
         ComposeQuadrantApp()
+        BusinessCardApp()
     }
 }
 
@@ -130,9 +137,11 @@ fun TaskManagerApp() {
 //COMPOSE QUADRANT APP
 @Composable
 fun ComposeQuadrantApp() {
-    Column(modifier = Modifier
-        .fillMaxWidth()
-        .height(LocalConfiguration.current.screenHeightDp.dp)) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(LocalConfiguration.current.screenHeightDp.dp)
+    ) {
         Row(modifier = Modifier.weight(1f)) {
             ComposableQuadrant(
                 title = stringResource(id = R.string.quad_one_title), description = stringResource(
@@ -149,9 +158,11 @@ fun ComposeQuadrantApp() {
         }
         Row(modifier = Modifier.weight(1f)) {
             ComposableQuadrant(
-                title = stringResource(id = R.string.quad_three_title), description = stringResource(
+                title = stringResource(id = R.string.quad_three_title),
+                description = stringResource(
                     id = R.string.quad_three_desc
-                ), backgroundColor = Color.Cyan,
+                ),
+                backgroundColor = Color.Cyan,
                 modifier = Modifier.weight(1f)
             )
             ComposableQuadrant(
@@ -188,4 +199,56 @@ fun ComposableQuadrant(
     }
 }
 
+@Composable
+fun BusinessCardApp() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(LocalConfiguration.current.screenHeightDp.dp).padding(bottom = 32.dp)
+    ) {
+        CardIntroduction(
+            name = stringResource(id = R.string.business_name),
+            title = stringResource(id = R.string.business_title),
+            profileImage = painterResource(id = R.drawable.picture),
+            modifier = Modifier.align(Alignment.Center)
+        )
+        CardLinks(
+            contact = stringResource(id = R.string.business_contact),
+            website = stringResource(id = R.string.business_website),
+            email = stringResource(id = R.string.business_email),
+            modifier = Modifier.align(Alignment.BottomCenter)
+        )
+    }
+}
+
+@Composable
+fun CardIntroduction(
+    name: String,
+    title: String,
+    profileImage: Painter,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter = profileImage, contentDescription = null)
+        Text(text = name, fontWeight = FontWeight.Bold, fontSize = 24.sp, modifier = Modifier.padding(vertical = 16.dp))
+        Text(text = title)
+    }
+}
+
+@Composable
+fun CardLinks(contact: String, website: String, email: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxWidth()) {
+        SingleCardLink(Icons.Rounded.Call, text = contact)
+        SingleCardLink(Icons.Rounded.Share, text = website)
+        SingleCardLink(Icons.Rounded.Email, text = email)
+    }
+}
+
+@Composable
+fun SingleCardLink(icon: ImageVector, text: String, modifier: Modifier = Modifier) {
+    Row(modifier = modifier.fillMaxWidth().padding(horizontal = 16.dp, vertical = 8.dp), horizontalArrangement = Arrangement.SpaceBetween) {
+        Icon(icon, contentDescription = null, tint = Color(0xFF3ddc84))
+        Text(text = text, color = Color(0xFF3ddc84))
+    }
+}
 
